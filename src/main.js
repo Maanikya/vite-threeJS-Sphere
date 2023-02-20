7d94509189155d4b6f7c93b5956490c94b399d8e
Maanikya <maanikya.acharya@gmail.com>
2023-02-20 23:45:43 +0530

    Added Scene

--- src/main.js
+++ src/main.js
@@ -9,5 +9,14 @@ const material = new THREE.MeshStandardMaterial({
     color: "#00ff83",
 })
 const mesh = new THREE.Mesh(geometry, material)
-
 scene.add(mesh);
+
+//Camera
+const camera = new THREE.PerspectiveCamera(45, 800, 600)
+scene.add(camera)
+
+//Renderer
+const canvas = document.querySelector('.webgl')
+const renderer = new THREE.WebGLRenderer({canvas})
+renderer.setSize(800, 600)
+renderer.render(scene, camera)
